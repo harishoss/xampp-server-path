85fe05c802c1747192fdc9a6ebded4c0f24f3789
hy505 <hariupwork88@gmail.com>
2019-05-22 16:38:04 +0530

    updated

--- xampp-root.py
+++ xampp-root.py
@@ -2,11 +2,11 @@
 import os
 from sys import argv
 from re import match
-from stat import S_IWUSR, S_IREAD
 import subprocess
 
-# httpd.conf path 
+# httpd.conf path
 HTTPD_CONF = '/opt/lampp/etc/httpd.conf'
+
 
 def get_httpd_content(file_path):
     ''' 
@@ -18,6 +18,7 @@ def get_httpd_content(file_path):
     httpd_file = open(file_path, 'r')
     return httpd_file.readlines()
 
+
 def is_matched(string):
     '''
     accepts a string.
@@ -25,10 +26,11 @@ def is_matched(string):
     regex pattern 'DocumentRoot ".+"'
     if not, false is returned.
     '''
-    if match('^DocumentRoot ".*"$',string):
+    if match('^DocumentRoot ".*"$', string):
         return True
     else:
         return False
+
 
 def replace_path(fileContent, replace_string):
     '''
@@ -41,12 +43,16 @@ def replace_path(fileContent, replace_string):
     for lineCount, line in enumerate(fileContent):
         if is_matched(line):
             if fileContent[lineCount+1] == '<Directory "{}">\n'.format(line.split('"')[1]):
-                fileContent[lineCount] = 'DocumentRoot "{}"\n'.format(replace_string)
-                fileContent[lineCount+1] = '<Directory "{}">\n'.format(replace_string)
+                fileContent[lineCount] = 'DocumentRoot "{}"\n'.format(
+                    replace_string)
+                fileContent[lineCount +
+                            1] = '<Directory "{}">\n'.format(replace_string)
                 return
             else:
-                exit("httpd.conf file is correcpted  - Could't found line starts with Directory")
-    exit("httpd.conf file is correcpted  - Could't found line starts with DocumentRoot")   
+                exit(
+                    "httpd.conf file is correcpted  - Could't found line starts with Directory")
+    exit("httpd.conf file is correcpted  - Could't found line starts with DocumentRoot")
+
 
 def save_to_file(fileContent):
     '''
@@ -55,10 +61,15 @@ def save_to_file(fileContent):
     (No appending, completely repalces 
     the file)
     '''
+    subprocess.call(['sudo', '-v'])
     with open(HTTPD_CONF, 'w') as file:
         file.writelines(fileContent)
         print('successfully replaced the path in httpd.conf')
-        subprocess.call(['sudo', '/opt/lampp/lampp', 'restart'])
+        if(argv[2] == 1):
+            subprocess.call(['sudo', '/opt/lampp/lampp', 'restart'])
+        else:
+            subprocess.call(['sudo', '/opt/lampp/lampp', 'start'])
+
 
 def process_argv():
     '''
@@ -66,8 +77,8 @@ def process_argv():
     returns path if necesary arguments 
     passsed.
     '''
-    if(len(argv) == 1):
-        exit("No Necessary Arguments found. Provide a 'path/to/replace' or '--current' as arguments.")
+    if(len(argv) != 3):
+        exit("No Necessary Arguments found. Provide a 'path/to/replace' or '--current'  and '1' or '0' as arguments.")
     if argv[1] == '--current':
         return os.getcwd()
     else:
@@ -75,6 +86,7 @@ def process_argv():
             return argv[1]
         else:
             exit("Given Path is not a directory or can't be accesable")
+
 
 if __name__ == "__main__":
     path_to_replace = process_argv()
